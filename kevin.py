09109e31fe56aeb97844938445bf562cfe60bd48
Juan Andres <andres090454@gmail.com>
2021-08-31 16:27:50 -0500

    miprimergit

---
+++ kevin.py
@@ -0,0 +1,15 @@
+defp pedir datos():
+    
+    print("cual es tu nombre")
+    
+    nombre=input()
+    
+    print("que edad tienes")
+    
+    edad=input()
+    
+    print("estudias o trabajas")
+    
+    estudias=input()
+    
+    trabajas=input()
